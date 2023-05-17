ced255fd57aae8a7a6e001f36390f7923355de81
Calin Dobos <calin.dobos@qwebsolutions.eu>
2023-05-17 14:40:27 +0300

    Pack nugets script.

---
+++ Scripts/pack_nugets.bat
@@ -0,0 +1,10 @@
+@echo off
+del nugets /s /q
+SET version=1.5.3
+set outputFolder=nugets
+echo Output folder: %outputFolder%
+echo Version: %version%
+dotnet pack ..\Metapsi.Runtime -o %outputFolder% /p:Version=%version% -c Release
+dotnet pack ..\Metapsi.Module -o %outputFolder% /p:Version=%version% -c Release
+dotnet pack ..\Metapsi.Hyperapp -o %outputFolder% /p:Version=%version% -c Release
+dotnet pack ..\Metapsi.Web -o %outputFolder% /p:Version=%version% -c Release
