f65097f7675c12d18c73c2ce7ce4f69e5969e1a2
lilizahar <zaharii.evelina@gmail.com>
2023-05-31 18:08:43 +0300

    Fixed path for generated css

--- Metapsi.Hyperapp/tailwind/tailwind.config.js
+++ Metapsi.Hyperapp/tailwind/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html", "../*.cs", "../Controls/*.cs", "../static/*.js", "../../Mds/MdsInfrastructure.Ui/*.cs", "../../Mds/MdsCommon.Ui/*.cs"],
+  content: ["index.html", "../*.cs", "../Controls/*.cs", "../static/*.js", "../../../Mds/MdsInfrastructure.Ui/*.cs", "../../../Mds/MdsCommon.Ui/*.cs"],
   theme: {
     extend: {},
   },
