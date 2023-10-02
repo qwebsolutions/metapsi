918c08c13d30fac3886f1e7cad5bf80526a04ba7
Calin Dobos <calin.dobos@qwebsolutions.eu>
2023-10-02 19:43:19 +0300

    A bit more details about the wrap-up example.

--- Metapsi.Tutorial/docs/articles/jump-in-wrap-up.md
+++ Metapsi.Tutorial/docs/articles/jump-in-wrap-up.md
@@ -3,3 +3,17 @@
 Now that we have a basic understanding of working with HTML, branching and looping, we can put it all together to create a slighly more useful control.
 
 CodeSample:WrapUpTable
+
+<div class="block-note">This sample uses the grid CSS classes from <a href="https://tailwindcss.com/docs/grid-template-rows" target="_blank">tailwind.css</a>.</div> 
+
+There are several things to notice:
+
+* The users are ordered descending by last login time. This works client side because the ISO 8601 date format allows correct sorting of string dates.
+* The zebra stripes are created by checking if the current index is even. CSS is just a string so it can be concatenated based on any condition you need to use.
+* <span class="inline-code">Optional</span> allows you to display content based on a specific condition
+* <span class="inline-code">FormatDateTime</span> formats the date using the browser locale
+
+
+#### Hope you had fun
+
+The advanced tutorial is coming soon. We'll cover setters (yes, surprisingly, that is quite an advanced topic), event handling, virtual DOM and more. Stay tuned.
--- Metapsi.Tutorial/docs/samples/WrapUpTable.cs
+++ Metapsi.Tutorial/docs/samples/WrapUpTable.cs
@@ -7,7 +7,7 @@ using System.Linq;
 namespace Metapsi.Tutorial.Samples;
 
 /// <summary>
-/// Table
+/// A grid of users
 /// </summary>
 public class WrapUpTable : TutorialSample<WrapUpTable.Model>
 {
@@ -27,72 +27,82 @@ public class WrapUpTable : TutorialSample<WrapUpTable.Model>
         var headerCss = "py-3 px-2 font-medium text-left bg-blue-400 text-white ";
         var baseRowCss = b.Const("py-3 px-2 font-light text-left text-gray-700 ");
 
-        var table = b.Div(
+        var grid = b.Div(
             "grid grid-cols-4 w-full p-4 rounded ",
             b => b.Div(headerCss, b => b.Text("User")),
             b => b.Div(headerCss, b => b.Text("Is logged in")),
             b => b.Div(headerCss + " col-span-2 ", b => b.Text("Last log in")));
 
         b.Foreach(
-            b.Get(model, x => x.AllUsers),
+            b.Get(
+                model, 
+                x => x.AllUsers.OrderByDescending(x=>x.LastLogIn).ToList()),
             (b, user, index) =>
             {
                 var rowCss = b.If(
                     b.Get(index, x => x % 2 == 0),
                     b => baseRowCss,
-                    b => b.Concat(baseRowCss, b.Const(" bg-gray-100 ")));
+                    b => b.Concat(
+                        baseRowCss, 
+                        b.Const(" bg-gray-100 ")));
 
-                b.Add(table, b.Div(rowCss, b => b.Text(b.Get(user, x => x.Name))));
-                b.Add(table, b.Div(rowCss, b => b.Optional(b.Get(user, x => x.IsLoggedIn), b => b.Text("✓", "text-green-500"))));
-                b.Add(table, b.Div(b.Concat(rowCss, b.Const(" col-span-2 ")), b => b.Text(b.FormatDatetime(b.Get(user, x => x.LastLogIn)))));
+                b.Add(grid, 
+                    b.Div(
+                        rowCss, 
+                        b => b.Text(
+                            b.Get(user, x => x.Name))));
+
+                b.Add(grid,
+                    b.Div(
+                        rowCss,
+                        b => b.Optional(
+                            b.Get(user, x => x.IsLoggedIn),
+                            b => b.Text("✓", "text-green-500"))));
+
+                b.Add(grid, 
+                    b.Div(
+                        b.Concat(
+                            rowCss, 
+                            b.Const(" col-span-2 ")), 
+                        b => b.Text(
+                            b.FormatDatetime(
+                                b.Get(user, x => x.LastLogIn)))));
             });
 
-        return table;
+        return grid;
     }
 
     public override Model GetSampleData()
     {
-        return new Model()
+        List<string> userNames = new()
         {
-            AllUsers = new List<Model.User>()
-            {
-                new Model.User()
-                {
-                    Name = "Denzel",
-                    IsLoggedIn = true,
-                    LastLogIn = DateTime.Now.AddDays(-1)
-                },
-                new Model.User()
-                {
-                    Name = "Orson",
-                    IsLoggedIn = false,
-                    LastLogIn = DateTime.Now.AddDays(-1)
-                },
-                new Model.User()
-                {
-                    Name = "Sybil",
-                    IsLoggedIn = true,
-                    LastLogIn = DateTime.Now.AddDays(-1)
-                },
-                new Model.User()
-                {
-                    Name = "Ulysses",
-                    IsLoggedIn = true,
-                    LastLogIn = DateTime.Now.AddDays(-1)
-                },
-                new Model.User()
-                {
-                    Name = "Phineas",
-                    IsLoggedIn = false,
-                    LastLogIn = DateTime.Now.AddDays(-1)
-                },
-                new Model.User()
-                {
-                    Name = "Neva",
-                    IsLoggedIn = true,
-                    LastLogIn = DateTime.Now.AddDays(-1)
-                }
-            }
+            "Denzel",
+            "Orson",
+            "Sybil",
+            "Ulysses",
+            "Phineas",
+            "Neva"
         };
+
+        var model = new Model() { AllUsers = new() };
+
+        var random = new Random();
+
+        foreach (var userName in userNames)
+        {
+            model.AllUsers.Add(new Model.User()
+            {
+                Name = userName,
+                LastLogIn = DateTime.Now.AddMinutes(new Random().Next(60 * 24) * -1)
+            });
+        }
+
+        // Make sure that some users are logged in, but not all of them
+        for (int i = 0; i < userNames.Count - 2; i++)
+        {
+            model.AllUsers[random.Next(userNames.Count)].IsLoggedIn = true;
+        }
+
+        return model;
     }
 }
