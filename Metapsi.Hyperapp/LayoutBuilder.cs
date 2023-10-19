74bf42e75e60195b7937e5dd0d115aac9ac6cc98
Calin <calin.dobos@qwebsolutions.eu>
2023-10-19 22:55:51 +0300

    Tabs.

--- Metapsi.Hyperapp/LayoutBuilder.cs
+++ Metapsi.Hyperapp/LayoutBuilder.cs
@@ -34,5 +34,30 @@ namespace Metapsi.Hyperapp
 
             return b.Render(controlDefinition, data);
         }
+
+        public static Var<IVNode> FromDefinition<TDefinition, TData>(
+            this LayoutBuilder b,
+            Func<TDefinition> init,
+            Var<TData> staticData,
+            Action<ControlBuilder<TDefinition, TData>, Var<TData>> customize = null)
+            where TDefinition : IControlDefinition<TData>, new()
+        {
+            TDefinition controlDefinition = init();
+            if (customize != null)
+            {
+                ControlBuilder<TDefinition, TData> controlBuilder = new(b, controlDefinition, staticData);
+                customize(controlBuilder, staticData);
+            }
+
+            return b.Render(controlDefinition, staticData);
+        }
+
+        public static Var<IVNode> Optional(this LayoutBuilder b, Var<bool> ifValue, System.Func<LayoutBuilder, Var<IVNode>> buildControl)
+        {
+            return b.If(
+                ifValue,
+                b => b.Call(buildControl),
+                b => b.H(ViewBuilder.VoidNodeTag, (b, props) => { }));
+        }
     }
 }
