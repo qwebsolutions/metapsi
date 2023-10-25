61bb1fd651670beb4ce10c74fa16ba410f58e063
Calin <calin.dobos@qwebsolutions.eu>
2023-10-25 19:27:44 +0300

    Parallel OpenAI processing. Disable send button when no message input. CallApi/CallServer effects.

---
+++ Metapsi.Hyperapp/Effects/CallServer.cs
@@ -0,0 +1,77 @@
+﻿using Metapsi.Syntax;
+using Metapsi.Ui;
+using static ServerExtensions;
+using System;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace Metapsi.Hyperapp
+{
+
+    public static class CallServerEffects
+    {
+        public static Var<HyperType.Effect> CallServer<TSyntaxBuilder, TState, TInputData, TPayload>(
+            this TSyntaxBuilder b,
+            Var<TInputData> inputData,
+            Var<TPayload> payload,
+            Func<CommandContext, TInputData, TPayload, System.Threading.Tasks.Task<TInputData>> onServerAction,
+            Var<HyperType.Action<TState, TInputData>> onResult,
+            Var<HyperType.Action<TState, ApiError>> onError)
+            where TSyntaxBuilder : SyntaxBuilder
+        {
+            var callServerPayload = b.MakeCallServerPayload(inputData, payload, onServerAction.Method, onResult, onError);
+
+            var def = b.Def<
+                SyntaxBuilder,
+                HyperType.Dispatcher<TState>,
+                CallApiInput<TState, ServerActionInput, ServerActionResponse>>(CallApiExtensions.CallApiEffecter<TState, ServerActionInput, ServerActionResponse>);
+
+            var effecter = b.MakeEffect<TState, CallApiInput<TState, ServerActionInput, ServerActionResponse>>(def, callServerPayload);
+            return effecter;
+        }
+
+        public static Var<HyperType.Effect> CallServer<TSyntaxBuilder, TState, TInputData>(
+            this TSyntaxBuilder b,
+            Var<TInputData> inputData,
+            Func<CommandContext, TInputData, System.Threading.Tasks.Task<TInputData>> onServerAction,
+            Var<HyperType.Action<TState, TInputData>> onResult,
+            Var<HyperType.Action<TState, ApiError>> onError)
+            where TSyntaxBuilder : SyntaxBuilder
+        {
+            var callServerPayload = b.MakeCallServerPayload(inputData, b.Const(string.Empty), onServerAction.Method, onResult, onError);
+            var def = b.Def<
+                SyntaxBuilder,
+                HyperType.Dispatcher<TState>,
+                CallApiInput<TState, ServerActionInput, ServerActionResponse>>(CallApiExtensions.CallApiEffecter<TState, ServerActionInput, ServerActionResponse>);
+
+            var effecter = b.MakeEffect<TState, CallApiInput<TState, ServerActionInput, ServerActionResponse>>(def, callServerPayload);
+            return effecter;
+        }
+
+        private static Var<CallApiInput<TState, ServerActionInput, ServerActionResponse>> MakeCallServerPayload<TState, TInputData, TPayload>(
+            this SyntaxBuilder b,
+            Var<TInputData> inputData,
+            Var<TPayload> payload,
+            System.Reflection.MethodInfo serverMethod,
+            Var<HyperType.Action<TState, TInputData>> onResult,
+            Var<HyperType.Action<TState, ApiError>> onError)
+        {
+            var serverActionInput = b.NewObj<Metapsi.Ui.ServerActionInput>();
+            b.Set(serverActionInput, x => x.SerializedModel, b.Serialize(inputData));
+            b.Set(serverActionInput, x => x.SerializedPayload, b.Serialize(payload));
+            b.Set(serverActionInput, x => x.HandlerMethod, b.Const(serverMethod.Name));
+            b.Set(serverActionInput, x => x.QualifiedHandlerClass, b.Const(serverMethod.DeclaringType.AssemblyQualifiedName));
+
+            var input = b.MakeCallApiEffecterPayload(
+                Metapsi.Ui.ServerActionEndpoint.ServerAction,
+                serverActionInput,
+                b.MakeAction((SyntaxBuilder b, Var<TState> model, Var<Metapsi.Ui.ServerActionResponse> result) =>
+                {
+                    var resultObject = b.Deserialize<TInputData>(b.Get(result, x => x.SerializedModel));
+                    return b.MakeActionDescriptor(onResult, resultObject);
+                }),
+                onError);
+
+            return input;
+        }
+    }
+}
--- Metapsi.Hyperapp/ServerExtensions.cs
+++ Metapsi.Hyperapp/ServerExtensions.cs
@@ -8,32 +8,32 @@ using System.Collections.Generic;
 
 public static class ServerExtensions
 {
-    public static Var<HyperType.Action<TState, TPayload>> MakeServerAction<TState, TPayload>(
-        this SyntaxBuilder b,
-        Var<TState> model,
-        Func<TState, TPayload, TState> onServerAction)
-        where TState : IApiSupportState
-    {
-        return MakeServerAction<TState, TPayload>(b, model, onServerAction.Method);
-    }
+//    public static Var<HyperType.Action<TState, TPayload>> MakeServerAction<TState, TPayload>(
+//        this SyntaxBuilder b,
+//        Var<TState> model,
+//        Func<TState, TPayload, TState> onServerAction)
+//        where TState : IApiSupportState
+//    {
+//        return MakeServerAction<TState, TPayload>(b, model, onServerAction.Method);
+//    }
 
-    public static Var<HyperType.Action<TState, TPayload>> MakeServerAction<TState, TPayload>(
-        this SyntaxBuilder b,
-        Var<TState> model,
-        Func<CommandContext, TState, TPayload, System.Threading.Tasks.Task<TState>> onServerAction)
-        where TState : IApiSupportState
-    {
-        return MakeServerAction<TState, TPayload>(b, model, onServerAction.Method);
-    }
+//    public static Var<HyperType.Action<TState, TPayload>> MakeServerAction<TState, TPayload>(
+//        this SyntaxBuilder b,
+//        Var<TState> model,
+//        Func<CommandContext, TState, TPayload, System.Threading.Tasks.Task<TState>> onServerAction)
+//        where TState : IApiSupportState
+//    {
+//        return MakeServerAction<TState, TPayload>(b, model, onServerAction.Method);
+//    }
 
-    public static Var<HyperType.Action<TState>> MakeServerAction<TState>(
-        this SyntaxBuilder b,
-        Var<TState> model,
-        Func<CommandContext, TState, System.Threading.Tasks.Task<TState>> onServerAction)
-        where TState : IApiSupportState
-    {
-        return MakeServerAction<TState>(b, model, onServerAction.Method);
-    }
+//    public static Var<HyperType.Action<TState>> MakeServerAction<TState>(
+//        this SyntaxBuilder b,
+//        Var<TState> model,
+//        Func<CommandContext, TState, System.Threading.Tasks.Task<TState>> onServerAction)
+//        where TState : IApiSupportState
+//    {
+//        return MakeServerAction<TState>(b, model, onServerAction.Method);
+//    }
 
     //public class CallApiPayload<TDataPayload>
     //{
@@ -41,73 +41,6 @@ public static class ServerExtensions
     //    public TDataPayload Payload { get; set; }
     //}
 
-    public class CallApiEffecterInput<TState, TPayload, TResult>
-    {
-        public string Url { get; set; }
-        public TPayload DataPayload { get; set; }
-        public HyperType.Action<TState, TResult> OnResult { get; set; }
-        public HyperType.Action<TState, ApiError> OnError { get; set; }
-    }
-
-    public static Var<CallApiEffecterInput<TState, TPayload, TResult>> MakeCallApiEffecterPayload<TState, TPayload, TResult>(
-        this SyntaxBuilder b,
-        Request<TResult, TPayload> request,
-        Var<TPayload> payload,
-        Var<HyperType.Action<TState, TResult>> onResult,
-        Var<HyperType.Action<TState, ApiError>> onError)
-    {
-        var callApiEffecterInput = b.NewObj<CallApiEffecterInput<TState, TPayload, TResult>>();
-
-        if (Api.AreScalarTypes(typeof(TPayload)))
-        {
-            b.Set(callApiEffecterInput, x => x.Url, b.Concat(b.Const($"/api/{request.Name}/"), payload.As<string>()));
-        }
-        else
-        {
-            b.Set(callApiEffecterInput, x => x.Url, b.Const($"/api/{request.Name}"));
-            b.Set(callApiEffecterInput, x => x.DataPayload, payload);
-        }
-        b.Set(callApiEffecterInput, x => x.OnResult, onResult);
-        b.Set(callApiEffecterInput, x => x.OnError, onError);
-
-        return callApiEffecterInput;
-    }
-
-    public static void CallApiEffecter<TState, TData, TResult>(
-        SyntaxBuilder b, 
-        Var<HyperType.Dispatcher<TState>> dispatcher, 
-        Var<CallApiEffecterInput<TState, TData, TResult>> callApiParameters)
-    {
-        var fetchOptions = b.NewObj<FetchOptions>();
-
-        b.If(
-            b.HasObject(
-                b.Get(callApiParameters, x => x.DataPayload)),
-            b =>
-            {
-                b.SetDynamic(b.Get(fetchOptions, x => x.headers), DynamicProperty.String("Content-Type"), b.Const("application/json"));
-                b.Set(fetchOptions, x => x.method, "POST");
-                var fetchPayload = b.NewObj<Api.PostBody<TData>>();
-                b.Set(fetchPayload, x => x.P1, b.Get(callApiParameters, x => x.DataPayload));
-                b.Set(fetchOptions, x => x.body, b.Serialize(fetchPayload));
-            },
-            b =>
-            {
-                b.Set(fetchOptions, x => x.method, "GET");
-            });
-
-        b.Fetch(
-            b.Get(callApiParameters, x => x.Url),
-            fetchOptions,
-            b.Def((SyntaxBuilder b, Var<TResult> result) =>
-            {
-                b.Dispatch(dispatcher, b.MakeActionDescriptor(b.Get(callApiParameters, x => x.OnResult), result));
-            }),
-            b.Def((SyntaxBuilder b, Var<ApiError> error) =>
-            {
-                b.Dispatch(dispatcher, b.MakeActionDescriptor(b.Get(callApiParameters, x => x.OnError), error));
-            }));
-    }
 
     //public static Var<Action<HyperType.Dispatcher<TState>, CallApiEffecterInput<TState, TData, TResult>>> MakeCallApiEffecter<TState, TData, TResult>(
     //    this SyntaxBuilder b)
@@ -179,103 +112,4 @@ public static class ServerExtensions
     //    return effectList.As<HyperType.Effect>();
     //}
 
-    public static Var<HyperType.Effect> CallServer<TSyntaxBuilder, TState, TInputData, TPayload>(
-        this TSyntaxBuilder b,
-        Var<TInputData> inputData,
-        Var<TPayload> payload,
-        Func<CommandContext, TInputData, TPayload, System.Threading.Tasks.Task<TInputData>> onServerAction,
-        Var<HyperType.Action<TState, TInputData>> onResult,
-        Var<HyperType.Action<TState, ApiError>> onError)
-        where TSyntaxBuilder: SyntaxBuilder
-    {
-        var serverActionInput = b.NewObj<Metapsi.Ui.ServerActionInput>();
-        b.Set(serverActionInput, x => x.SerializedModel, b.Serialize(inputData));
-        b.Set(serverActionInput, x => x.SerializedPayload, b.Serialize(payload));
-        b.Set(serverActionInput, x => x.HandlerMethod, b.Const(onServerAction.Method.Name));
-        b.Set(serverActionInput, x => x.QualifiedHandlerClass, b.Const(onServerAction.Method.DeclaringType.AssemblyQualifiedName));
-
-        var input = b.MakeCallApiEffecterPayload(
-            Metapsi.Ui.ServerActionEndpoint.ServerAction, 
-            serverActionInput, 
-            b.MakeAction((SyntaxBuilder b, Var<TState> model, Var<Metapsi.Ui.ServerActionResponse> result) =>
-            {
-                var resultObject = b.Deserialize<TInputData>(b.Get(result, x => x.SerializedModel));
-                return b.MakeActionDescriptor(onResult, resultObject);
-            }), 
-            onError);
-
-        var def = b.Def<
-            SyntaxBuilder,
-            HyperType.Dispatcher<TState>,
-            CallApiEffecterInput<TState, ServerActionInput, ServerActionResponse>>(CallApiEffecter<TState, ServerActionInput, ServerActionResponse>);
-
-        var effecter = b.MakeEffect<TState, CallApiEffecterInput<TState, ServerActionInput, ServerActionResponse>>(def, input);
-        //b.MakeCallApiEffecter().As<HyperType.Effecter<CallApiEffecterInput<TState, ServerActionInput, Metapsi.Ui.ServerActionResponse>>>();
-
-        return effecter;
-        //b.Log("effecter", effecter);
-
-        //return b.MakeEffectWithPayload<CallApiEffecterInput<TState, ServerActionInput, Metapsi.Ui.ServerActionResponse>>(effecter, input);
-    }
-
-    private static Var<HyperType.Action<TState, TPayload>> MakeServerAction<TState, TPayload>(
-            this SyntaxBuilder b,
-            Var<TState> model,
-            System.Reflection.MethodInfo method)
-            where TState : IApiSupportState
-    {
-        var clientAction = b.MakeAction((SyntaxBuilder b, Var<TState> state, Var<TPayload> payload) =>
-        {
-            var serverActionInput = b.NewObj<Metapsi.Ui.ServerActionInput>();
-            b.Set(serverActionInput, x => x.SerializedModel, b.Serialize(state));
-            b.Set(serverActionInput, x => x.SerializedPayload, b.Serialize(payload));
-            b.Set(serverActionInput, x => x.HandlerMethod, b.Const(method.Name));
-            b.Set(serverActionInput, x => x.QualifiedHandlerClass, b.Const(method.DeclaringType.AssemblyQualifiedName));
-
-            return b.MakeStateWithEffects(
-                b.ShowPanel(model),
-                b.MakeEffect(
-                    b.Def(
-                        b.Request(
-                            Metapsi.Ui.ServerActionEndpoint.ServerAction,
-                            serverActionInput,
-                            b.MakeAction((SyntaxBuilder b, Var<TState> model, Var<Metapsi.Ui.ServerActionResponse> result) =>
-                            {
-                                return b.Deserialize<TState>(b.Get(result, x => x.SerializedModel));
-                            })))));
-        });
-
-        return clientAction;
-    }
-
-    private static Var<HyperType.Action<TState>> MakeServerAction<TState>(
-        this SyntaxBuilder b,
-        Var<TState> model,
-        System.Reflection.MethodInfo method)
-        where TState : IApiSupportState
-    {
-        var clientAction = b.MakeAction((SyntaxBuilder b, Var<TState> state) =>
-        {
-            var serverActionInput = b.NewObj<Metapsi.Ui.ServerActionInput>();
-            b.Set(serverActionInput, x => x.SerializedModel, b.Serialize(state));
-            b.Set(serverActionInput, x => x.HandlerMethod, b.Const(method.Name));
-            b.Set(serverActionInput, x => x.QualifiedHandlerClass, b.Const(method.DeclaringType.AssemblyQualifiedName));
-
-            return b.MakeStateWithEffects(
-                b.ShowPanel(model),
-                b.MakeEffect(
-                    b.Def(
-                        b.Request(
-                            Metapsi.Ui.ServerActionEndpoint.ServerAction,
-                            serverActionInput,
-                            b.MakeAction((SyntaxBuilder b, Var<TState> model, Var<Metapsi.Ui.ServerActionResponse> result) =>
-                            {
-                                var newModel = b.Deserialize<TState>(b.Get(result, x => x.SerializedModel));
-                                b.DispatchEvent(b.Const("sharedStateUpdate"), newModel);
-                                return newModel;
-                            })))));
-        });
-
-        return clientAction;
-    }
 }
