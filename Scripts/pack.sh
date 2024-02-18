e8eb1d829fb9874efaeee444a2cd0b3566d98ee5
Calin <calin.dobos@qwebsolutions.eu>
2024-02-18 20:33:01 +0200

    Removed commented commands in pack.sh

--- Scripts/pack.sh
+++ Scripts/pack.sh
@@ -3,7 +3,6 @@
 REPO_URL=$(git config --get remote.origin.url)
 REPO_BRANCH=$(git branch --show-current)
 REPO_COMMIT=$(git rev-parse HEAD)
-#dotnet build -p:RepositoryUrl="$REPO_URL" -p:RepositoryBranch="$REPO_BRANCH" -p:RepositoryCommit="$REPO_COMMIT" -p:RepositoryType="git"
 
 rm ./nugets -r
 version=1.7.0
@@ -22,9 +21,5 @@ dotnet pack ../Metapsi.Shoelace -o $outputFolder -c Release -p:Version="$version
 dotnet pack ../Metapsi.Heroicons -o $outputFolder -c Release -p:Version="$version" -p:RepositoryUrl="$REPO_URL" -p:RepositoryBranch="$REPO_BRANCH" -p:RepositoryCommit="$REPO_COMMIT" -p:RepositoryType="git"
 dotnet pack ../Metapsi.Redis -o $outputFolder -c Release -p:Version="$version" -p:RepositoryUrl="$REPO_URL" -p:RepositoryBranch="$REPO_BRANCH" -p:RepositoryCommit="$REPO_COMMIT" -p:RepositoryType="git"
 dotnet pack ../Metapsi.Messaging -o $outputFolder -c Release -p:Version="$version" -p:RepositoryUrl="$REPO_URL" -p:RepositoryBranch="$REPO_BRANCH" -p:RepositoryCommit="$REPO_COMMIT" -p:RepositoryType="git"
-
-#dotnet pack ..\Metapsi.Hyperapp -o %outputFolder% /p:Version=%version% -c Release
-#dotnet pack ..\Metapsi.Web -o %outputFolder% /p:Version=%version% -c Release
-#dotnet pack ..\Metapsi.Redis -o %outputFolder% /p:Version=%version% -c Release
-#dotnet pack ..\Metapsi.Sqlite -o %outputFolder% /p:Version=%version% -c Release
-#dotnet pack ..\Metapsi.Timer -o %outputFolder% /p:Version=%version% -c Release
+dotnet pack ../Metapsi.SQLite -o $outputFolder -c Release -p:Version="$version" -p:RepositoryUrl="$REPO_URL" -p:RepositoryBranch="$REPO_BRANCH" -p:RepositoryCommit="$REPO_COMMIT" -p:RepositoryType="git"
+dotnet pack ../Metapsi.Ui -o $outputFolder -c Release -p:Version="$version" -p:RepositoryUrl="$REPO_URL" -p:RepositoryBranch="$REPO_BRANCH" -p:RepositoryCommit="$REPO_COMMIT" -p:RepositoryType="git"
