57900e44159797a8247a048793ce95533e3e51c5
Calin <calin.dobos@qwebsolutions.eu>
2024-10-01 22:15:51 +0300

    Event queue.

---
+++ Metapsi.Runtime/EventQueue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Metapsi
+{
+    public class EventQueue
+    {
+        private TaskQueue<List<Func<object, Task>>> handlersQueue = new TaskQueue<List<Func<object, Task>>>(new List<Func<object, Task>>());
+
+        public void Raise<T>(T e)
+        {
+            var _ = handlersQueue.Enqueue(async (handlers) =>
+            {
+                foreach (var handler in handlers)
+                {
+                    await handler(e);
+                }
+            });
+        }
+
+        public void On<T>(Func<T, Task> handle)
+        {
+            var _ = handlersQueue.Enqueue(async (handlers) =>
+            {
+                handlers.Add(async (object e) =>
+                {
+                    if (e is T match)
+                    {
+                        await handle(match);
+                    }
+                });
+            });
+        }
+    }
+}
