249a05ac49722a270bb89787a21a00b891add64d
Calin <calin.dobos@qwebsolutions.eu>
2025-06-03 17:30:31 +0300

    .

---
+++ Metapsi.WebComponentGenerator/src/index.ts
@@ -0,0 +1,13 @@
+export * from "./CSharpContracts.js"
+export * from './CSharpOutput.js'
+export * from './CSharpSystemTypes.js'
+export * from './CSharpWebComponentContracts.js'
+export * from './ClientSideRendering.js'
+export * from './ServerSideRendering.js'
+export * from './TypeParser.js'
+
+
+// import * as genShoelace from './GenerateShoelace.js'
+
+// await genShoelace.GenerateShoelace("2.20.1", "C:\\github\\qwebsolutions\\metapsi\\Metapsi.Shoelace\\Controls");
+
