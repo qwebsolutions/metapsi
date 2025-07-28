9fd2f47ced7b5049e9b528d24520f16c5082b407
Calin <calin.dobos@qwebsolutions.eu>
2025-07-28 12:45:46 +0300

    DOMTokenList API

--- Metapsi.Html/Dom/Element.cs
+++ Metapsi.Html/Dom/Element.cs
@@ -8,49 +8,142 @@ namespace Metapsi.Html;
 public interface Element : Node
 {
     /// <summary>
-    /// The id property of the Element interface represents the element's identifier, reflecting the id global attribute.
+    /// The Element.childElementCount read-only property returns the number of child elements of this element.
     /// </summary>
-    public string id { get; set; }
+    int childElementCount { get; }
+
+    /// <summary>
+    /// The read-only children property returns a live HTMLCollection which contains all of the child elements of the element upon which it was called. 
+    /// Element.children includes only element nodes.To get all child nodes, including non-element nodes like text and comment nodes, use Node.childNodes.
+    /// </summary>
+    HTMLCollection children { get; }
+
+    /// <summary>
+    /// The Element.classList is a read-only property that returns a live DOMTokenList collection of the class attributes of the element. This can then be used to manipulate the class list.
+    /// </summary>
+    DOMTokenList classList { get; }
+
+    /// <summary>
+    /// The className property of the Element interface gets and sets the value of the class attribute of the specified element.
+    /// </summary>
+    string className { get; set; }
+
+    /// <summary>
+    /// The clientHeight read-only property of the Element interface is zero for elements with no CSS or inline layout boxes; otherwise, it's the inner height of an element in pixels. It includes padding but excludes borders, margins, and horizontal scrollbars (if present).
+    /// clientHeight can be calculated as: CSS height + CSS padding - height of horizontal scrollbar(if present).
+    /// When clientHeight is used on the root element(the &lt;html&gt; element), (or on &lt;body&gt; if the document is in quirks mode), the viewport's height (excluding any scrollbar) is returned.
+    /// </summary>
+    int clientHeight { get; }
+
+    /// <summary>
+    /// The clientLeft read-only property of the Element interface returns the width of the left border of an element in pixels. It includes the width of the vertical scrollbar if the text direction of the element is right-to-left and if there is an overflow causing a left vertical scrollbar to be rendered. clientLeft does not include the left margin or the left padding.
+    /// </summary>
+    /// <remarks>Note: When an element has display: inline, clientLeft returns 0 regardless of the element's border.</remarks>
+    int clientLeft { get; }
+
+    /// <summary>
+    /// The clientTop read-only property of the Element interface returns the width of the top border of an element in pixels.
+    /// All that lies between the offsetTop and clientTop is the element's border. This is because the offsetTop indicates the location of the top of the border (not the margin) while the client area starts immediately below the border, including padding. Therefore, the clientTop value is always equal to the border-top-width, rounded to integer. For example, if the computed border-top-width is zero, then clientTop is also zero.
+    /// </summary>
+    int clientTop { get; }
+
+    /// <summary>
+    /// The clientWidth read-only property of the Element interface is zero for inline elements and elements with no CSS; otherwise, it's the inner width of an element in pixels. It includes padding but excludes borders, margins, and vertical scrollbars (if present).
+    /// When clientWidth is used on the root element(the &lt;html&gt; element), (or on &lt;body&gt; if the document is in quirks mode), the viewport's width (excluding any scrollbar) is returned.
+    /// </summary>
+    int clientWidth { get; }
 
     /// <summary>
     /// The Element.firstElementChild read-only property returns an element's first child Element, or null if there are no child elements.
+    /// Element.firstElementChild includes only element nodes.To get all child nodes, including non-element nodes like text and comment nodes, use Node.firstChild.
     /// </summary>
     Element firstElementChild { get; }
 
     /// <summary>
+    /// The id property of the Element interface represents the element's identifier, reflecting the id global attribute.
+    /// </summary>
+    string id { get; set; }
+
+    /// <summary>
     /// The Element property innerHTML gets or sets the HTML or XML markup contained within the element.
     /// </summary>
-    public string innerHTML { get; set; }
+    string innerHTML { get; set; }
+
+    /// <summary>
+    /// The Element.lastElementChild read-only property returns an element's last child Element, or null if there are no child elements.
+    /// Element.lastElementChild includes only element nodes.To get all child nodes, including non-element nodes like text and comment nodes, use Node.lastChild.
+    /// </summary>
+    Element lastElementChild { get; }
+
+    /// <summary>
+    /// The Element.localName read-only property returns the local part of the qualified name of an element.
+    /// </summary>
+    string localName { get; }
+
+    /// <summary>
+    /// The Element.namespaceURI read-only property returns the namespace URI of the element, or null if the element is not in a namespace.
+    /// </summary>
+    string namespaceURI { get; }
+
+    /// <summary>
+    /// The Element.nextElementSibling read-only property returns the element immediately following the specified one in its parent's children list, or null if the specified element is the last one in the list.
+    /// </summary>
+    string nextElementSibling { get; }
 
     /// <summary>
     /// The outerHTML attribute of the Element DOM interface gets the serialized HTML fragment describing the element including its descendants. It can also be set to replace the element with nodes parsed from the given string.
     /// </summary>
-    public string outerHTML { get; set; }
+    string outerHTML { get; set; }
+
+    /// <summary>
+    /// The part property of the Element interface represents the part identifier(s) of the element (i.e., set using the part attribute), returned as a DOMTokenList. These can be used to style parts of a shadow DOM, via the ::part pseudo-element.
+    /// </summary>
+    DOMTokenList part { get; set; }
+
+    /// <summary>
+    /// The Element.prefix read-only property returns the namespace prefix of the specified element, or null if no prefix is specified.
+    /// </summary>
+    string prefix { get; }
 
     /// <summary>
     /// The Element.previousElementSibling read-only property returns the Element immediately prior to the specified one in its parent's children list, or null if the specified element is the first one in the list.
     /// </summary>
-    public Element previousElementSibling { get;  }
+    Element previousElementSibling { get;  }
 
     /// <summary>
-    /// The Element.scrollTop property gets or sets the number of pixels by which an element's content is scrolled from its top edge
+    /// The scrollHeight read-only property of the Element interface is a measurement of the height of an element's content, including content not visible on the screen due to overflow.
     /// </summary>
-    public decimal scrollTop { get; set; }
+    int scrollHeight { get; }
+
+    /// <summary>
+    /// The scrollLeft property of the Element interface gets or sets the number of pixels by which an element's content is scrolled from its left edge. This value is subpixel precise in modern browsers, meaning that it isn't necessarily a whole number.
+    /// </summary>
+    decimal scrollLeft { get; set; }
+
+    /// <summary>
+    /// The scrollTop property of the Element interface gets or sets the number of pixels by which an element's content is scrolled from its top edge. This value is subpixel precise in modern browsers, meaning that it isn't necessarily a whole number.
+    /// </summary>
+    decimal scrollTop { get; set; }
+
+    /// <summary>
+    /// The scrollWidth read-only property of the Element interface is a measurement of the width of an element's content, including content not visible on the screen due to overflow.
+    /// </summary>
+    int scrollWidth { get; }
 
     /// <summary>
     /// The Element.shadowRoot read-only property represents the shadow root hosted by the element.
     /// </summary>
-    public ShadowRoot shadowRoot { get; }
+    ShadowRoot shadowRoot { get; }
 
     /// <summary>
     /// The slot property of the Element interface returns the name of the shadow DOM slot the element is inserted in.
     /// </summary>
-    public string slot { get; set; }
+    string slot { get; set; }
 
     /// <summary>
     /// The tagName read-only property of the Element interface returns the tag name of the element on which it's called.
     /// </summary>
-    public string tagName { get; }
+    string tagName { get; }
 }
 
 /// <summary>
