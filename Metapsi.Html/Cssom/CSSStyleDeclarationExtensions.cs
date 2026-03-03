d194cf7f2be445d893908998b88e5471cf24634e
Calin <calin.dobos@qwebsolutions.eu>
2026-03-03 18:52:55 +0200

    Inherit CSSStyleDeclaration methods.

--- Metapsi.Html/Cssom/CSSStyleDeclarationExtensions.cs
+++ Metapsi.Html/Cssom/CSSStyleDeclarationExtensions.cs
@@ -13,7 +13,8 @@ public static class CSSStyleDeclarationExtensions
     /// <param name="b"></param>
     /// <param name="property">A string representing the property name to be checked.</param>
     /// <returns>A string that represents the priority (e.g., "important") if one exists. If none exists, returns the empty string.</returns>
-    public static ObjBuilder<string> getPropertyPriority(this ObjBuilder<CSSStyleDeclaration> b, Var<string> property)
+    public static ObjBuilder<string> getPropertyPriority<T>(this ObjBuilder<T> b, Var<string> property)
+        where T: CSSStyleDeclaration
     {
         return b.Call<string>(nameof(getPropertyPriority), property);
     }
@@ -24,7 +25,8 @@ public static class CSSStyleDeclarationExtensions
     /// <param name="b"></param>
     /// <param name="property">A string representing the property name (in hyphen case) to be checked.</param>
     /// <returns>A string containing the value of the property. If not set, returns the empty string. The property value is dynamically computed, not what was originally specified in the declaration.</returns>
-    public static ObjBuilder<string> getPropertyValue(this ObjBuilder<CSSStyleDeclaration> b, Var<string> property)
+    public static ObjBuilder<string> getPropertyValue<T>(this ObjBuilder<T> b, Var<string> property)
+        where T : CSSStyleDeclaration
     {
         return b.Call<string>(nameof(getPropertyValue), property);
     }
@@ -35,7 +37,8 @@ public static class CSSStyleDeclarationExtensions
     /// <param name="b"></param>
     /// <param name="index">The index of the node to be fetched. The index is zero-based.</param>
     /// <returns>A string that is the name of the CSS property at the specified index.</returns>
-    public static ObjBuilder<string> item(this ObjBuilder<CSSStyleDeclaration> b, Var<int> index)
+    public static ObjBuilder<string> item<T>(this ObjBuilder<T> b, Var<int> index)
+        where T : CSSStyleDeclaration
     {
         return b.Call<string>(nameof(item), index);
     }
@@ -46,7 +49,8 @@ public static class CSSStyleDeclarationExtensions
     /// <param name="b"></param>
     /// <param name="property">A string representing the property name to be removed. Multi-word property names are hyphenated (kebab-case) and not camel-cased.</param>
     /// <returns>A string equal to the value of the CSS property before it was removed.</returns>
-    public static ObjBuilder<string> removeProperty(this ObjBuilder<CSSStyleDeclaration> b, Var<string> property)
+    public static ObjBuilder<string> removeProperty<T>(this ObjBuilder<T> b, Var<string> property)
+        where T : CSSStyleDeclaration
     {
         return b.Call<string>(nameof(removeProperty), property);
     }
@@ -58,7 +62,8 @@ public static class CSSStyleDeclarationExtensions
     /// <param name="propertyName">A string representing the CSS property name (hyphen case) to be modified.</param>
     /// <param name="value">A string containing the new property value. If not specified, treated as the empty string. A null value is treated the same as the empty string ("").</param>
     /// <remarks>value must not contain "!important", that should be set using the priority parameter.</remarks>
-    public static void setProperty(this ObjBuilder<CSSStyleDeclaration> b, Var<string> propertyName, Var<string> value)
+    public static void setProperty<T>(this ObjBuilder<T> b, Var<string> propertyName, Var<string> value)
+        where T : CSSStyleDeclaration
     {
         b.Call(nameof(setProperty), propertyName, value);
     }
@@ -74,7 +79,8 @@ public static class CSSStyleDeclarationExtensions
     /// <para>"", undefined, or null for removing the !important flag if present.</para>
     /// </param>
     /// <remarks>value must not contain "!important", that should be set using the priority parameter.</remarks>
-    public static void setProperty(this ObjBuilder<CSSStyleDeclaration> b, Var<string> propertyName, Var<string> value, Var<string> priority)
+    public static void setProperty<T>(this ObjBuilder<T> b, Var<string> propertyName, Var<string> value, Var<string> priority)
+        where T : CSSStyleDeclaration
     {
         b.Call(nameof(setProperty), propertyName, value, priority);
     }
