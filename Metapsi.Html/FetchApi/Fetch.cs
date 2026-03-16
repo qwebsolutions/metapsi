cc292aa44299134e23494de05a142db711dbef47
Calin <calin.dobos@qwebsolutions.eu>
2026-03-16 10:57:08 +0200

    Fetch overload with RequestInit

--- Metapsi.Html/FetchApi/Fetch.cs
+++ Metapsi.Html/FetchApi/Fetch.cs
@@ -38,6 +38,19 @@ public static partial class FetchExtensions
     /// </summary>
     /// <param name="b"></param>
     /// <param name="resource">This defines the resource that you wish to fetch.</param>
+    /// <param name="options">A RequestInit object containing any custom settings that you want to apply to the request.</param>
+    /// <returns>A Promise that resolves to a Response object.</returns>
+    public static Var<Promise> Fetch(this SyntaxBuilder b, Var<string> resource, Var<RequestInit> options)
+    {
+        return b.CallOnObject<Promise>(b.Self(), "fetch", resource, options);
+    }
+
+    /// <summary>
+    /// The fetch() method starts the process of fetching a resource from the network, returning a promise that is fulfilled once the response is available.
+    /// The promise resolves to the Response object representing the response to your request.
+    /// </summary>
+    /// <param name="b"></param>
+    /// <param name="resource">This defines the resource that you wish to fetch.</param>
     /// <param name="setOptions">A RequestInit builder containing any custom settings that you want to apply to the request.</param>
     /// <returns>A Promise that resolves to a Response object.</returns>
     public static Var<Promise> Fetch(this SyntaxBuilder b, Var<string> resource, Action<PropsBuilder<RequestInit>> setOptions)
