8780f91b89f36c025f6b46102c932808ad322d94
Calin <calin.dobos@qwebsolutions.eu>
2026-06-29 16:11:19 +0300

    Bind to int on IonInput and IonSelect

--- Metapsi.Ionic/Binding.cs
+++ Metapsi.Ionic/Binding.cs
@@ -21,6 +21,21 @@ public static partial class IonInputControl
             return b.Clone(model);
         }));
     }
+
+    public static void BindTo<TEntity>(
+        this PropsBuilder<IonInput> b,
+        Var<TEntity> entityRef,
+        System.Linq.Expressions.Expression<System.Func<TEntity, int>> onProperty)
+    {
+        var value = b.Get(entityRef, onProperty);
+        b.SetValue(value);
+        b.OnIonInput(b.MakeAction((SyntaxBuilder b, Var<object> model, Var<CustomEvent<InputInputEventDetail>> e) =>
+        {
+            var value = b.Get(e, x => x.detail.value).As<int>();
+            b.Set(entityRef, onProperty, value);
+            return b.Clone(model);
+        }));
+    }
 }
 
 public static partial class IonTextareaControl
@@ -53,6 +68,21 @@ public static partial class IonSelectControl
         b.OnIonChange(b.MakeAction((SyntaxBuilder b, Var<object> model, Var<CustomEvent<SelectChangeEventDetail>> e) =>
         {
             var value = b.Get(e, x => x.detail.value).As<string>();
+            b.Set(entityRef, onProperty, value);
+            return b.Clone(model);
+        }));
+    }
+
+    public static void BindTo<TEntity>(
+        this PropsBuilder<IonSelect> b,
+        Var<TEntity> entityRef,
+        System.Linq.Expressions.Expression<System.Func<TEntity, int>> onProperty)
+    {
+        var value = b.Get(entityRef, onProperty);
+        b.SetValue(value);
+        b.OnIonChange(b.MakeAction((SyntaxBuilder b, Var<object> model, Var<CustomEvent<SelectChangeEventDetail>> e) =>
+        {
+            var value = b.Get(e, x => x.detail.value).As<int>();
             b.Set(entityRef, onProperty, value);
             return b.Clone(model);
         }));
